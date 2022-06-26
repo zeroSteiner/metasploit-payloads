501c0eb8220666ee79efb0c8f0b192e67b5bab6e
Spencer McIntyre <zeroSteiner@gmail.com>
2022-06-26 11:51:11 -0400

    Rename the example from foobar to example

---
+++ python/meterpreter/ext_server_example.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+ERROR_SUCCESS            = 0
+TLV_EXTENSIONS           = 20000
+TLV_META_TYPE_STRING     = (1 << 16)
+
+TLV_TYPE_EXAMPLE_ECHO_MSG = TLV_META_TYPE_STRING | (TLV_EXTENSIONS + 1)
+
+meterpreter.register_extension('example')
+
+# Meterpreter register function decorators
+register_function = meterpreter.register_function
+
+@register_function
+def example_echo(request, response):
+    message = packet_get_tlv(request, TLV_TYPE_EXAMPLE_ECHO_MSG)['value']
+    response += tlv_pack(TLV_TYPE_EXAMPLE_ECHO_MSG, message)
+    return ERROR_SUCCESS, response
